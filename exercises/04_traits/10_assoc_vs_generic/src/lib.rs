406aac9e4d0022bcf0f3a85a6463bfaa6c6ce879
Katherine Hough <kmhough@amazon.com>
2025-02-11 22:19:03 +0000

    * Chapter 4 solutions

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -12,6 +12,35 @@
 // interested in learning more about it.
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
+pub trait Power<Exp = Self> {
+    type Output;
+
+    fn power(&self, n: Exp) -> Self::Output;
+}
+
+impl Power<u16> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: u16) -> u32 {
+        self.pow(n as u32)
+    }
+}
+
+impl Power<u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: u32) -> u32 {
+        self.pow(n)
+    }
+}
+
+impl Power<&u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: &u32) -> u32 {
+        self.pow(*n)
+    }
+}
 
 #[cfg(test)]
 mod tests {
